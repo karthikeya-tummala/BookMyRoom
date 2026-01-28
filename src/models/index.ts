ca185e3f7dc745986240c403b5d836491ab23d44
karthikeya-tummala <karthikeyatummala@gmail.com>
2026-01-28 12:14:18 +0530

    Add index for model exports

---
+++ src/models/index.ts
@@ -0,0 +1,9 @@
+import { employee } from "./Employee.model.js";
+import { booking } from "./Booking.model.js";
+import { room } from "./Room.model.js";
+
+export {
+  employee,
+  booking,
+  room
+};
